b61686acd852697baf242ea4e030bf396c6079ef
Drake Davis <drake.davis@fedex.com>
2024-08-14 17:28:15 -0400

    Fixing a variable

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_machine_run_command" "example" {
   name               = "Setup PowerBI Gateway"
-  location           = azurerm_resource_group.example.location
+  location           = var.location
   virtual_machine_id = var.virtual_machine_id
   source {
     script = file("${path.module}/files/install-gateway.ps1")
